9b8cafb555f68fe3d89126234c2dba6ecae9705e
195410008ayurizky <ayurizky876@gmail.com>
2022-03-26 12:27:04 +0700

    pertemuan 7

---
+++ minggu-07/src/Class_Objects.py
@@ -0,0 +1,16 @@
+class MyClass:
+    """A simple example class"""
+    i = 12345
+
+    def f(self):
+        return 'hello world'
+
+class Complex:
+     def __init__(self, realpart, imagpart):
+         self.r = realpart
+         self.i = imagpart
+
+x = Complex(3.0, -4.5)
+x.r, x.i
+#output
+(3.0, -4.5)
---
+++ minggu-07/src/Class_and_Instance_Variables.py
@@ -0,0 +1,61 @@
+class Dog:
+
+    kind = 'canine'         # class variable shared by all instances
+
+    def __init__(self, name):
+        self.name = name    # instance variable unique to each instance
+
+d = Dog('Fido')
+e = Dog('Buddy')
+d.kind                  # shared by all dogs
+#output
+'canine'
+e.kind                  # shared by all dogs
+#output
+'canine'
+d.name                  # unique to d
+#output
+'Fido'
+e.name                  # unique to e
+#output
+'Buddy'
+
+
+class Dog:
+
+    tricks = []             # mistaken use of a class variable
+
+    def __init__(self, name):
+        self.name = name
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+d = Dog('Fido')
+e = Dog('Buddy')
+d.add_trick('roll over')
+e.add_trick('play dead')
+d.tricks                # unexpectedly shared by all dogs
+#output
+['roll over', 'play dead']
+
+
+class Dog:
+
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []    # creates a new empty list for each dog
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+d = Dog('Fido')
+e = Dog('Buddy')
+d.add_trick('roll over')
+e.add_trick('play dead')
+d.tricks
+#output
+['roll over']
+e.tricks
+#output
+['play dead']
